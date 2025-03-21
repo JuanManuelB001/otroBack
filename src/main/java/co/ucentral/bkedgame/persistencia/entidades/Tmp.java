e40bed7a4403a9398d5688bc671fb1bf7b08c7ed
Juan Manuel Bolivar <jbolivarm2@ucentral.edu.co>
2025-03-21 18:23:21 -0500

    fetch(backend) Agregando Atributos a la  Entidad Equipo

--- src/main/java/co/ucentral/bkedgame/persistencia/entidades/Tmp.java
+++ src/main/java/co/ucentral/bkedgame/persistencia/entidades/Tmp.java
@@ -1,19 +1,20 @@
 package co.ucentral.bkedgame.persistencia.entidades;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.text.DateFormat;
 
 @Entity
+@Table(name = "Equipos")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tmp {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(nullable = false)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String nombre, nombreCorto;
+    private DateFormat fechaCreacion;
 }
